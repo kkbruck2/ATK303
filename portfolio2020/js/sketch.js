e5807b2e5642130184becf0ae6b797369f6a0084
Kim Brucker <kkbruck2@ilstu.edu>
2020-01-31 08:37:33 -0600

    portfolio 2020

---
+++ portfolio2020/js/sketch.js
@@ -0,0 +1,40 @@
+//javascript sketch
+var debugInput = document.querySelector("input");
+function updateDebugState() {
+    document.body.classList.toggle('debug-on', debugInput.checked);
+}
+debugInput.addEventListener("click", updateDebugState);
+updateDebugState();
+
+/*
+* Fix sidebar at some point and remove
+* fixed position at content bottom
+*/
+/*
+* Fix sidebar at some point and remove
+* fixed position at content bottom
+*/
+$(window).scroll(function () {
+	var headerHeight = $('.site-header').innerHeight();
+	var contentHeight = $('.site-main').innerHeight();
+	var sidebarHeight = $('.side-navigation').height();
+  var sidebarBottomPos = contentHeight - sidebarHeight;
+  var trigger = $(window).scrollTop() - headerHeight;
+
+      	if ($(window).scrollTop() >= headerHeight) {
+          	$('.side-navigation').addClass('fixed');
+      	} else {
+          	$('.side-navigation').removeClass('fixed');
+      	}
+
+      	if (trigger >= sidebarBottomPos) {
+          	$('.side-navigation').addClass('bottom');
+      	} else {
+          	$('.side-navigation').removeClass('bottom');
+      	}
+});
+
+/*
+* Fix sidebar at some point and remove
+* fixed position at content bottom
+*/
